23fe2e2cc9147fed3ad8f776d866da2947dd1c56
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-01-06 00:07:12 +0300

    test ok

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function well(x){
-    // In this kata you need to check the provided array (x) 
-    // for good ideas 'good' and bad ideas 'bad'. 
-    // If there are one or two good ideas, return 'Publish!', 
-    // if there are more than 2 return 'I smell a series!'. 
-    // If there are no good ideas, as is often the case, return 'Fail!
+function well(x) {
+    let num = 0;
+    for (let i = 0; i < x.length; i++) {
+        x[i] === 'good' ? num++ : num;
     }
+    if (num > 2) {
+        return 'I smell a series!';
+    } else if (num >= 1 && num <= 2) {
+        return 'Publish!';
+    } else {
+        return 'Fail!';
+    }
+}
